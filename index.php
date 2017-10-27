2a47b3579f485719a872f6b941b4c1cc7ae7bb58
admin <admin@iMac3.local>
2017-10-27 15:44:44 +0200

    templated at best i guess and working on content

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
   require_once("templates/head.html");
   require_once("templates/body.html");
+  require_once("pages/work.html");
+  require_once("pages/skills.html");
+  require_once("pages/about.html");
   require_once("templates/foot.html");
