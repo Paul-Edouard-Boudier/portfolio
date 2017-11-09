87d38739e92bb87caaab080f82083fbc067a7b57
admin <admin@iMac3-15.local>
2017-11-09 14:29:47 +0100

    added the template to work with i18n, just need to follow this and add an input to get the locale and it's all good

---
+++ i18n/fr.php
@@ -0,0 +1,14 @@
+<?php
+  $locale = $_GET['locale'];
+
+  $filetrad = 'i18n/'. $locale .'.php';
+  require_once $filetrad;
+
+
+  $i18n = [
+    'templates' => [
+      'head' => [
+        'work' => "Mon travail"
+      ]
+    ],
+  ];
