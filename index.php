bf74e9eb15e9d68ca49d54449f85ec115a787ff8
admin <admin@iMac3-15.local>
2017-11-10 08:57:42 +0100

    changed extension of file

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  require_once("templates/head.html");
+  require_once("templates/head.php");
   require_once("templates/body.html");
   require_once("pages/work.html");
   require_once("pages/skills.html");
